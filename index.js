f247812ff5e315b318aa6e219efa19fa4ab81802
Kiddvverde <leonardo.mainkra@gmail.com>
2025-11-25 18:54:56 -0600

    feat: add API consume on button, fixed urlencoded, add new get

--- index.js
+++ index.js
@@ -10,12 +10,23 @@ const API_URL = "https://api.coinpaprika.com/v1/";
 // Middlewares
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(express.bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+app.get("/coins", async (req, res) => {
+    try {
+        const response = await axios.get(`${API_URL}coins`);
+        const coins = response.data;
+        console.log(coins);
+        res.render("index.ejs", { coins });
+    } catch (error) {
+        console.error("Error fetching coins:", error);
+        res.status(500).send("Error fetching coins");
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
