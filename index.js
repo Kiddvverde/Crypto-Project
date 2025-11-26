61eb72c449e1ae3d140ebb07631f7160061fd4b8
Kiddvverde <leonardo.mainkra@gmail.com>
2025-11-25 18:23:35 -0600

    feat: add home view, configure middlewares and integrate TailwindCSS
    - Added index.ejs view
    - Configured view engine and static files
    - Integrated TailWindCSS into index view

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+// Importes
 import express from "express";
 import axios from "axios";
 
+// Config Express
 const app = express();
 const PORT = 3000;
+const API_URL = "https://api.coinpaprika.com/v1/";
 
-app.get("/", async (req, res) => {
-    try {
-        res.render("index.ejs");
-    } catch (error) {
-        res.status(500).send(`Parece que hubo un errror!: ${error.message}`);
-    }
+// Middlewares
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
